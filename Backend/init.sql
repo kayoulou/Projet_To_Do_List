7c12472d9d5811ffdf520ca1739f7c95bdc44341
Manue <manuellaouedraogo275@gmail.com>
2025-06-12 17:25:29 +0000

    modification des fichiers de configs pour le bouton ajouter

--- Backend/init.sql
+++ Backend/init.sql
@@ -2,7 +2,7 @@
 
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
-CREATE TABLE IF NOT EXISTS T_todo (
+CREATE TABLE IF NOT EXISTS "T_todo" (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   title TEXT NOT NULL,
   statut TEXT DEFAULT 'à faire',
--- frontend/src/axiosConfig.js
+++ frontend/src/axiosConfig.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
-const baseURL = import.meta.env.VITE_API_URL || ''; 
+const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
+ 
 // En prod, VITE_API_URL n’est pas défini donc baseURL = '' → appels relatifs
 console.log("API URL (prod) =", baseURL);
 const instance = axios.create({
